4eab3ad94468406401bf7489902f08e4528c07af
yourevo <yourevo089@gmail.com>
2025-08-29 20:04:55 +0600

    Update Help.js

--- Script/commands/Help.js
+++ Script/commands/Help.js
@@ -94,11 +94,11 @@ module.exports.run = function ({ api, event, args, getText }) {
 
  const imgPath = __dirname + "/cache/helppic.jpg";
  const callback = () => api.sendMessage({ body: text, attachment: fs.createReadStream(imgPath) }, threadID, () => fs.unlinkSync(imgPath), messageID);
- return request("https://i.imgur.com/0D2V25h.jpeg").pipe(fs.createWriteStream(imgPath)).on("close", () => callback());
+ return request("https://drive.google.com/file/d/1UX4PIOcgMoQiqV6FP8jo9K5OZkWtw41P/view?usp=drivesdk").pipe(fs.createWriteStream(imgPath)).on("close", () => callback());
  }
 
  const detail = getText("moduleInfo", command.config.name, "1", "1", "1", `┃ ✪ ${command.config.name} - ${command.config.description}`, prefix);
  const imgPath = __dirname + "/cache/helppic.jpg";
  const callback = () => api.sendMessage({ body: detail, attachment: fs.createReadStream(imgPath) }, threadID, () => fs.unlinkSync(imgPath), messageID);
- return request("https://i.imgur.com/sxSn1K3.jpeg").pipe(fs.createWriteStream(imgPath)).on("close", () => callback());
+ return request("https://drive.google.com/file/d/1UX4PIOcgMoQiqV6FP8jo9K5OZkWtw41P/view?usp=drivesdk").pipe(fs.createWriteStream(imgPath)).on("close", () => callback());
 };
