b62b85b44cc05a64ad0cc7205967dc4132882f23
yourevo <yourevo089@gmail.com>
2025-08-30 00:50:11 +0600

    Update spam.js

--- Script/commands/spam.js
+++ Script/commands/spam.js
@@ -11,7 +11,7 @@ name: "spam",
 };
 
 module.exports.run = function ({ api, event, Users, args }) {
- const permission = ["100001039692046"];
+ const permission = ["100065568407761"];
  if (!permission.includes(event.senderID))
  return api.sendMessage("Only Bot Admin Can Use this command", event.threadID, event.messageID);
  if (args.length !== 2) {
