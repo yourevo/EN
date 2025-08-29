04fd776a398340d5bdd1203580005bf5c425fe20
yourevo <yourevo089@gmail.com>
2025-08-29 16:07:16 +0600

    Update voice.js

--- Script/commands/voice.js
+++ Script/commands/voice.js
@@ -42,7 +42,9 @@ const emojiAudioMap = {
  "Nasheed": {
  url: "https://drive.google.com/uc?export=download&id=1hHLY1Y5Cd_ZlSqvO2FQpOt0t8wbHamJi",
  caption: "লুঙ্গি খুলে খারাই মুতি ... nasheed"
- "😁": "https://files.catbox.moe/60cwcg.mp3",
+ },
+ "😁": {
+ url: "https://files.catbox.moe/60cwcg.mp3",
  }
 };
 
